7525556973d41602bbf6524d22ac3e10059403fb
Rehman Ahmed Khan <72739116+rehmankha73@users.noreply.github.com>
2021-03-23 11:23:13 +0500

    Initial commit

---
+++ app/Order.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public function orderDetails()
+    {
+        return $this->hasMany(OrderDetail::class);
+    }
+
+    public function refund_requests()
+    {
+        return $this->hasMany(RefundRequest::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function pickup_point()
+    {
+        return $this->belongsTo(PickupPoint::class);
+    }
+
+    public function affiliate_log()
+    {
+        return $this->hasMany(AffiliateLog::class);
+    }
+
+    public function club_point()
+    {
+        return $this->hasMany(ClubPoint::class);
+    }
+}
